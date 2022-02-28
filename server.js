aa8c9c7929702ce4a888824b27f75936901ad0c6
JuandiM <juandimena34@gmail.com>
2022-02-28 18:17:01 +0100

    prepare for deploy

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ const express = require ('express');
 const router = require ('express').Router ();
 const dotenv = require ('dotenv').config ();
 const properties = require ('./routes/properties');
+const path = require ('path');
 
 const app = express ();
+
+app.use (express.static (path.join (__dirname, 'client', 'build')));
 
 //DB CONNECTION
 const mongoose = require ('mongoose');
 
-mongoose
+/* mongoose
   .connect ('mongodb://localhost/properties_web_app', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then (() => console.log ('Connected to MongoDB'))
-  .catch (error => console.log (error));
+  .catch (error => console.log (error)); */
+
+mongoose
+  .connect (process.env.MONGODB_URI, {useNewUrlParser: true})
+  .then (() => console.log ('connected to Mongo'))
+  .catch (err => console.log (err));
 
 //Cors
 const cors = require ('cors');
@@ -34,5 +42,9 @@ app.use ('/api/properties', properties);
   const {property_id} = req.params;
   Property.findById (property_id).then (property => res.send (property));
 }); */
+
+app.get ('*', (req, res) => {
+  res.sendFile (path.join (__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen (5000, () => console.log ('SERVER running'));
