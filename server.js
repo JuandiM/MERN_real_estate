b0e9d04070ed810e0bb806e5554ec93a2a55ec1b
JuandiM <juandimena34@gmail.com>
2022-02-28 18:52:16 +0100

    fix server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require ('path');
 
 const app = express ();
 
+const port = process.env.PORT || 5000;
+
 app.use (express.static (path.join (__dirname, 'client', 'build')));
 
 //DB CONNECTION
@@ -47,4 +49,4 @@ app.get ('*', (req, res) => {
   res.sendFile (path.join (__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen (5000, () => console.log ('SERVER running'));
+app.listen (port, () => console.log ('SERVER running'));
