0c1577ba4597211beb32801d4060a1938d76404f
JuandiM <juandimena34@gmail.com>
2022-03-01 11:07:56 +0100

    pck change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = require ('express').Router ();
-require ('dotenv').config ();
+const dotenv = require ('dotenv').config ();
 const properties = require ('./routes/properties');
 
 //Cors
